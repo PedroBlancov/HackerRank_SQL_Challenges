a34acc6d2f9ca66ed0ccdfeb1507f08ef06a1484
Pedro  Blanco <104238746+PedroBlancov@users.noreply.github.com>
2022-10-27 19:28:57 -0500

    Update odd_even.py

--- odd_even.py
+++ odd_even.py
@@ -10,13 +10,15 @@ if __name__ == '__main__':
     n = int(input().strip())
 
 def task():
-    if n%2==1:
-        print ("Weird")
-    elif n%2==0 and (n>=2 or n<=5):
-        print ("Not Weird")
-    elif n%2==0 and (n>=6 or n<=20):
-        print ("Weird")
-    elif n%2==0 and (n>20):
-        print ("Not Weird")
+    if n%2!=0:
+       print("Weird")
+    else:
+        if n>=2 and n<=5:
+            print("Not Weird")
+        elif n>=6 and n<=20:
+            print("Weird")
+        elif n>20:
+            print("Not Weird")
     
 result= task()
+

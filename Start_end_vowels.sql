1abb66648ffde603c52cc6b0be5506962404b651
Pedro  Blanco <104238746+PedroBlancov@users.noreply.github.com>
2024-03-01 21:37:01 -0500

    Update Start_end_vowels.sql

--- Start_end_vowels.sql
+++ Start_end_vowels.sql
@@ -5,3 +5,17 @@ WHERE
 REGEXP_LIKE(city,'^a|^e|^i|^o|^u','i') AND
 REGEXP_LIKE(city,'a$|e$|i$|o$|u$','i')
 ;
+
+
+select distinct
+city
+from station
+WHERE
+city NOT LIKE 'A%'
+AND city NOT LIKE 'E%'
+AND city NOT LIKE 'I%'
+AND city NOT LIKE 'O%'
+AND city NOT LIKE 'U%'
+--(city,'a$|e$|i$|o$|u$', 'i')
+ORDER BY 1 ASC
+;
